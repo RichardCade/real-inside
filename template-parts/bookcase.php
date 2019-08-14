ce40165d37b24b8387e85c3fd20a2995e02b51f2
Richard Cade <richardcade@live.co.uk>
2019-08-14 08:37:20 +0200

    Backup

---
+++ template-parts/bookcase.php
@@ -0,0 +1,252 @@
+         <ul id="bk-list" class="bk-list clearfix">
+
+           <li>
+             <a href="<?php echo esc_url(site_url('/hr-portal')) ?>">
+               <div class="bk-book book-3">
+                 <div class="bk-front">
+                   <div class="bk-cover-back"></div>
+                   <div class="bk-cover">
+                     <h2>
+                       <span>HR Portal</span>
+                     </h2>
+                   </div>
+                 </div>
+                 <div class="bk-back">
+                 </div>
+                 <div class="bk-right"></div>
+                 <div class="bk-left">
+                   <h2>
+                     <span>HR Portal</span>
+                   </h2>
+                 </div>
+                 <div class="bk-top"></div>
+                 <div class="bk-bottom"></div>
+               </div>
+             </a>
+           </li>
+
+           <li>
+             <a href="<?php echo esc_url(site_url('/it-portal')) ?>">
+               <div class="bk-book book-3">
+                 <div class="bk-front">
+                   <div class="bk-cover-back"></div>
+                   <div class="bk-cover">
+                     <h2>
+                       <span>IT portal</span>
+                     </h2>
+                   </div>
+                 </div>
+                 <div class="bk-back">
+                 </div>
+                 <div class="bk-right"></div>
+                 <div class="bk-left">
+                   <h2>
+                     <span>IT portal</span>
+                   </h2>
+                 </div>
+                 <div class="bk-top"></div>
+                 <div class="bk-bottom"></div>
+               </div>
+             </a>
+           </li>
+
+           <li>
+             <a href="<?php echo esc_url(site_url('/marketing-portal')) ?>">
+               <div class="bk-book book-3">
+                 <div class="bk-front">
+                   <div class="bk-cover-back"></div>
+                   <div class="bk-cover">
+                     <h2>
+                       <span>Marketing portal</span>
+                     </h2>
+                   </div>
+                 </div>
+                 <div class="bk-back">
+                 </div>
+                 <div class="bk-right"></div>
+                 <div class="bk-left">
+                   <h2>
+                     <span>Marketing portal</span>
+                   </h2>
+                 </div>
+                 <div class="bk-top"></div>
+                 <div class="bk-bottom"></div>
+               </div>
+             </a>
+           </li>
+
+           <li>
+             <a href="<?php echo esc_url(site_url('/planning-portal')) ?>">
+               <div class="bk-book book-3">
+                 <div class="bk-front">
+                   <div class="bk-cover-back"></div>
+                   <div class="bk-cover">
+                     <h2>
+                       <span>Planning portal</span>
+                     </h2>
+                   </div>
+                 </div>
+                 <div class="bk-back">
+                 </div>
+                 <div class="bk-right"></div>
+                 <div class="bk-left">
+                   <h2>
+                     <span>Planning portal</span>
+                   </h2>
+                 </div>
+                 <div class="bk-top"></div>
+                 <div class="bk-bottom"></div>
+               </div>
+             </a>
+           </li>
+
+           <li>
+             <a href="<?php echo esc_url(site_url('/property-portal')) ?>">
+               <div class="bk-book book-3">
+                 <div class="bk-front">
+                   <div class="bk-cover-back"></div>
+                   <div class="bk-cover">
+                     <h2>
+                       <span>Property portal</span>
+                     </h2>
+                   </div>
+                 </div>
+                 <div class="bk-back">
+                 </div>
+                 <div class="bk-right"></div>
+                 <div class="bk-left">
+                   <h2>
+                     <span>Property portal</span>
+                   </h2>
+                 </div>
+                 <div class="bk-top"></div>
+                 <div class="bk-bottom"></div>
+               </div>
+             </a>
+           </li>
+
+           <li>
+             <a href="<?php echo esc_url(site_url('/public-relations-portal')) ?>">
+               <div class="bk-book book-3">
+                 <div class="bk-front">
+                   <div class="bk-cover-back"></div>
+                   <div class="bk-cover">
+                     <h2>
+                       <span>public relations portal</span>
+                     </h2>
+                   </div>
+                 </div>
+                 <div class="bk-back">
+                 </div>
+                 <div class="bk-right"></div>
+                 <div class="bk-left">
+                   <h2>
+                     <span>public relations portal</span>
+                   </h2>
+                 </div>
+                 <div class="bk-top"></div>
+                 <div class="bk-bottom"></div>
+               </div>
+             </a>
+           </li>
+
+           <li>
+             <a href="<?php echo esc_url(site_url('/strategy-portal')) ?>">
+               <div class="bk-book book-3">
+                 <div class="bk-front">
+                   <div class="bk-cover-back"></div>
+                   <div class="bk-cover">
+                     <h2>
+                       <span>strategy portal</span>
+                     </h2>
+                   </div>
+                 </div>
+                 <div class="bk-back">
+                 </div>
+                 <div class="bk-right"></div>
+                 <div class="bk-left">
+                   <h2>
+                     <span>strategy portal</span>
+                   </h2>
+                 </div>
+                 <div class="bk-top"></div>
+                 <div class="bk-bottom"></div>
+               </div>
+             </a>
+           </li>
+
+           <li>
+             <a href="<?php echo esc_url(site_url('/tradesman-portal')) ?>">
+               <div class="bk-book book-3">
+                 <div class="bk-front">
+                   <div class="bk-cover-back"></div>
+                   <div class="bk-cover">
+                     <h2>
+                       <span>tradesman portal</span>
+                     </h2>
+                   </div>
+                 </div>
+                 <div class="bk-back">
+                 </div>
+                 <div class="bk-right"></div>
+                 <div class="bk-left">
+                   <h2>
+                     <span>tradesman portal</span>
+                   </h2>
+                 </div>
+                 <div class="bk-top"></div>
+                 <div class="bk-bottom"></div>
+               </div>
+             </a>
+           </li>
+
+           <li>
+             <a href="<?php echo esc_url(site_url('/technology-portal')) ?>">
+               <div class="bk-book book-3">
+                 <div class="bk-front">
+                   <div class="bk-cover-back"></div>
+                   <div class="bk-cover">
+                     <h2>
+                       <span>technology portal</span>
+                     </h2>
+                   </div>
+                 </div>
+                 <div class="bk-back">
+                 </div>
+                 <div class="bk-right"></div>
+                 <div class="bk-left">
+                   <h2>
+                     <span>technology portal</span>
+                   </h2>
+                 </div>
+                 <div class="bk-top"></div>
+                 <div class="bk-bottom"></div>
+               </div>
+             </a>
+           </li>
+
+           <li>
+             <a href="<?php echo esc_url(site_url('/utilities-portal')) ?>">
+               <div class="bk-book book-3">
+                 <div class="bk-front">
+                   <div class="bk-cover-back"></div>
+                   <div class="bk-cover">
+                     <h2>
+                       <span>utilities portal</span>
+                     </h2>
+                   </div>
+                 </div>
+                 <div class="bk-back">
+                 </div>
+                 <div class="bk-right"></div>
+                 <div class="bk-left">
+                   <h2>
+                     <span>utilities portal</span>
+                   </h2>
+                 </div>
+                 <div class="bk-top"></div>
+                 <div class="bk-bottom"></div>
+               </div>
+             </a>
+           </li>
+         </ul>
