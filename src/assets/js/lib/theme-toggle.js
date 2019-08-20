ddf39e4c9023724583a13cc22259e0907b309780
Richard Cade <richardcade@live.co.uk>
2019-08-20 07:14:48 +0200

    clearing unused CSS

--- src/assets/js/lib/theme-toggle.js
+++ src/assets/js/lib/theme-toggle.js
@@ -1,4 +1,26 @@
+var themeSwitch = document.getElementById('themeSwitch');
+if (themeSwitch) {
+  initTheme();
+  
+  themeSwitch.addEventListener('change', function (event) {
+    resetTheme(); // update color theme
+  });
 
- document.getElementById('themeSwitch').addEventListener('change', function(event){
-(event.target.checked) ? document.getElementById("body").classList.add('dark') : document.getElementById("body").classList.remove('dark');
-});
+  function initTheme() {
+    var darkThemeSelected = (localStorage.getItem('themeSwitch') !== null && localStorage.getItem('themeSwitch') === 'dark');
+    // update checkbox
+    themeSwitch.checked = darkThemeSelected;
+    // update body data-theme attribute
+    darkThemeSelected ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark');
+  };
+  
+  function resetTheme() {
+    if (themeSwitch.checked) { // dark theme has been selected
+      document.querySelector('body').classList.add('dark');
+      localStorage.setItem('themeSwitch', 'dark'); // save theme selection 
+    } else {
+      document.querySelector('body').classList.remove('dark');
+      localStorage.removeItem('themeSwitch'); // reset theme selection 
+    }
+  };
+}
